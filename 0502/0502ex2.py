fbef465704134652660360b43c24de46f38bf227
김태환 <89343442+tkdwk1@users.noreply.github.com>
2023-05-02 13:22:47 +0900

    Add files via upload

---
+++ 0502/0502ex2.py
@@ -0,0 +1,22 @@
+'''
+2023-05-02
+김태환
+range()함수-순차적인 범위의 숫자 생성하는 함수
+'''
+
+for i in range(10): #반복 조건
+    print(i,end=' ') #반복 조건 참인 동안 반복
+
+print() #한 줄 띄우기
+
+for i in range(1,10): #반복 조건
+    print(i,end=' ')
+
+print()
+
+for i in range(1,11,2): #반복 조건
+    print(i,end=' ') #홀수 출력
+
+print()
+for i in range(10,0,-1):
+    print(i,end=' ')
