7f089e1f1230c04b4c699f02554b1c21f0612194
김태환 <89343442+tkdwk1@users.noreply.github.com>
2023-06-13 13:25:02 +0900

    Add files via upload

---
+++ 0613/0613ex3.py
@@ -0,0 +1,10 @@
+'''
+김태환
+사용자 정의 모듈 호출
+'''
+import polyArea
+
+w=float(input("사각형의 가로"))
+d=float(input("사각형의 세로"))
+
+print("사각형의 넓이:",polyArea.rectangleArea(w,d))
---
+++ 0613/ployArea.py
@@ -0,0 +1,19 @@
+"""
+2023-06-13
+김태환
+
+"""
+pi=3.14159
+
+def rectangelArea(width,depth):
+    return width*depth
+
+def triangleArea(base,height):
+    return base*height/2
+
+def circleArea(r):
+    return pi*r*r
+
+def circumference(r):
+    return 2*pi*r
+
